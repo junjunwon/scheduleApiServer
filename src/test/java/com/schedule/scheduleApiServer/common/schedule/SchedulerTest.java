14bab094f80faae8a84243f0226a441102561b46
won junho <wnsgh1993@gmail.com>
2022-08-19 22:33:46 +0900

    기본 schedule 테스트 구성 및 junit5 라이브러리 추가

---
+++ src/test/java/com/schedule/scheduleApiServer/common/schedule/SchedulerTest.java
@@ -0,0 +1,26 @@
+package com.schedule.scheduleApiServer.common.schedule;
+
+import com.schedule.scheduleApiServer.ScheduleApiServerApplication;
+import org.awaitility.Duration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+@SpringJUnitConfig(ScheduleApiServerApplication.class)
+public class SchedulerTest {
+
+    @SpyBean
+    Scheduler scheduler;
+
+    @Test
+    public void whenWaitOneSecond_thenScheduledIsCalledAtLeastTenTimes() {
+        await()
+                .atMost(Duration.ONE_MINUTE)
+                .untilAsserted(() -> verify(scheduler, atLeast(4)).getFileListScheduler());
+    }
+}
