801ab98bf9e14222c8a2c159bc42877dcb212fcf
won junho <wnsgh1993@gmail.com>
2022-08-19 22:09:14 +0900

    default properties 세팅

--- src/main/java/com/schedule/scheduleApiServer/ScheduleApiServerApplication.java
+++ src/main/java/com/schedule/scheduleApiServer/ScheduleApiServerApplication.java
@@ -3,14 +3,23 @@ package com.schedule.scheduleApiServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ScheduleApiServerApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+"classpath:application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(ScheduleApiServerApplication.class, args);
+		new SpringApplicationBuilder(ScheduleApiServerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+						.run(args);
+//		SpringApplication.run(ScheduleApiServerApplication.class, args);
 	}
 
 }
