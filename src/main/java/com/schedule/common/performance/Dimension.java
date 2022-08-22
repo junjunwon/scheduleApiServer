e0c36047ddd7d319dcf9b8186fbc6646bb8fba31
won junho <wnsgh1993@gmail.com>
2022-08-22 21:57:11 +0900

    [feature-rateLimit] API 호출 임계치 설정 - bucket4j
    실제 적용은 rest api crud를 작성하면서 진행할 계획

---
+++ src/main/java/com/schedule/common/performance/Dimension.java
@@ -0,0 +1,12 @@
+package com.schedule.common.performance;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Dimension {
+    private int length;
+    private int breadth;
+}
---
+++ src/main/java/com/schedule/common/performance/Perimeter.java
@@ -0,0 +1,15 @@
+package com.schedule.common.performance;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Perimeter {
+    private String shape;
+    private Double perimeter;
+}
