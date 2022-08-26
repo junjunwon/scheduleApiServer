daa5d48dd8c2188e2889812416072994581da8b2
won junho <wnsgh1993@gmail.com>
2022-08-26 22:32:11 +0900

    [feature-test] test코드 작성

---
+++ src/test/java/com/schedule/web/account/UserInfoControllerTest.java
@@ -0,0 +1,107 @@
+package com.schedule.web.account;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.schedule.domain.account.UserInfo;
+import com.schedule.domain.account.UserInfoRepository;
+import com.schedule.domain.file.FileInfo;
+import com.schedule.dto.account.UserInfoSaveRequestDto;
+import com.schedule.web.file.FileApiControllerTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserInfoControllerTest {
+
+    private static final Logger logger = LogManager.getLogger(UserInfoControllerTest.class);
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @AfterEach
+    public void cleanup() {
+        userInfoRepository.deleteAll();
+    }
+
+    @BeforeAll
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser(roles="USER")
+    public void USER_등록된다() throws Exception {
+        String username = "testUser";
+        String password = "testUser";
+        String role = "ROLE_USER";
+
+        UserInfoSaveRequestDto requestDto = UserInfoSaveRequestDto.builder()
+                .username(username)
+                .password(password)
+                .role(role)
+                .build();
+
+        String url = "http://localhost:"+port+"/api/createUser";
+
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        ResponseEntity<Long> responseEntity = restTemplate
+                .postForEntity(url, requestDto, Long.class);
+
+
+
+        assertThat(responseEntity.getStatusCode(), is(equalTo(HttpStatus.OK)));
+        assertThat(responseEntity.getBody(), greaterThan(0L));
+
+        List<UserInfo> all = userInfoRepository.findAll();
+
+        assertThat(all.get(0).getUsername(), is(equalTo(username)));
+        assertThat(all.get(0).getPassword(), is(equalTo(password)));
+        assertThat(all.get(0).getRole(), is(equalTo(role)));
+    }
+
+}
