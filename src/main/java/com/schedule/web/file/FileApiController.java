1f98cc4ba6b93b9b114cd513e27009b4a130e2d9
won junho <wnsgh1993@gmail.com>
2022-09-11 15:49:22 +0900

    공통 예외처리 적용 및 테스트 케이스 작성
    유저 및 파일 저장, null값 전송 시 exception 처리 테스트 등을 작성

--- src/main/java/com/schedule/web/file/FileApiController.java
+++ src/main/java/com/schedule/web/file/FileApiController.java
@@ -5,6 +5,7 @@ import com.schedule.common.response.Response;
 import com.schedule.dto.file.FileInfoResponseDto;
 import com.schedule.dto.file.FileInfoSaveRequestDto;
 import com.schedule.dto.file.FileInfoUpdateRequestDto;
+import com.schedule.exception.BucketExceedException;
 import com.schedule.service.file.FileService;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
@@ -20,6 +21,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.List;
@@ -48,10 +50,6 @@ public class FileApiController {
     @GetMapping("/fileList/list")
     public ResponseEntity<Response> getFileList() {
 
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
-
         if(bucket.tryConsume(1)) {
             Response response = new Response();
             List<FileInfoResponseDto> dtoList = fileService.findFileList();
@@ -60,27 +58,15 @@ public class FileApiController {
             response.setData(dtoList);
             response.setAvailableTokens(bucket.getAvailableTokens()); // 임계치까지 남은 api호출 횟수
             logger.info("success");
-            return new ResponseEntity<>(response, headers, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
-
-        logger.info("too many requests");
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        throw new BucketExceedException("title", "요청 횟수를 초과하였습니다.");
     }
 
     @PostMapping("/file/save")
-    public ResponseEntity<?> save(@Validated @RequestBody final FileInfoSaveRequestDto fileInfoSaveRequestDto, BindingResult bindingResult) {
-
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+    public ResponseEntity<?> save(@Valid @RequestBody final FileInfoSaveRequestDto fileInfoSaveRequestDto) {
 
         if(bucket.tryConsume(1)) {
-
-            //유효성 검사
-            if(bindingResult.hasErrors()) {
-                List<String> errors = bindingResult.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
-                return ResponseEntity.ok(new ErrorResponse("404", "Validation failure", errors));
-                // or 404 request
-            }
 
             Response response = new Response();
 
@@ -94,29 +80,18 @@ public class FileApiController {
             response.setAvailableTokens(bucket.getAvailableTokens()); // 임계치까지 남은 api호출 횟수
 
             logger.info("success");
-            return new ResponseEntity<>(response, headers, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
 
         }
 
-        logger.info("too many requests");
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        throw new BucketExceedException("title", "요청 횟수를 초과하였습니다.");
     }
 
     @PutMapping("/file/update/{id}")
     public ResponseEntity<?> updateContentById(@PathVariable Long id,
-                                                      @Validated @RequestBody final FileInfoUpdateRequestDto fileInfoUpdateRequestDto,
-                                                      BindingResult bindingResult) {
-
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+                                               @Validated @RequestBody final FileInfoUpdateRequestDto fileInfoUpdateRequestDto) {
 
         if(bucket.tryConsume(1)) {
-            //유효성 검사
-            if(bindingResult.hasErrors()) {
-                List<String> errors = bindingResult.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
-                // TODO : ExceptionHandler로 처리하기.
-                return ResponseEntity.ok(new ErrorResponse("404", "Validation failure", errors));
-            }
 
             Response response = new Response();
             Long index = fileService.updateContentById(id, fileInfoUpdateRequestDto);
@@ -129,18 +104,14 @@ public class FileApiController {
             response.setAvailableTokens(bucket.getAvailableTokens()); // 임계치까지 남은 api호출 횟수
 
             logger.info("success");
-            return new ResponseEntity<>(response, headers, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
-        logger.info("too many requests");
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        throw new BucketExceedException("title", "요청 횟수를 초과하였습니다.");
     }
 
 
     @DeleteMapping("/file/delete/{ids}")
     public ResponseEntity<Response> deleteContentsById(@PathVariable List<Long> ids) {
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
         if(bucket.tryConsume(1)) {
             Response response = new Response();
             long check = fileService.deleteContentsByIds(ids);
@@ -153,10 +124,10 @@ public class FileApiController {
             response.setAvailableTokens(bucket.getAvailableTokens()); // 임계치까지 남은 api호출 횟수
 
             logger.info("success");
-            return new ResponseEntity<>(response, headers, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
 
-        logger.info("too many requests");
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        throw new BucketExceedException("title", "요청 횟수를 초과하였습니다.");
     }
 }
+
