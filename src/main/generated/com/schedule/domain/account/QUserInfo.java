b4016217ff2c97afd642bc1949e9bd4b785f0142
won junho <wnsgh1993@gmail.com>
2022-09-11 16:31:17 +0900

    queryDSL QClass 경로 변경

---
+++ src/main/generated/com/schedule/domain/account/QUserInfo.java
@@ -0,0 +1,43 @@
+package com.schedule.domain.account;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUserInfo is a Querydsl query type for UserInfo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUserInfo extends EntityPathBase<UserInfo> {
+
+    private static final long serialVersionUID = 90401338L;
+
+    public static final QUserInfo userInfo = new QUserInfo("userInfo");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath password = createString("password");
+
+    public final StringPath role = createString("role");
+
+    public final StringPath username = createString("username");
+
+    public QUserInfo(String variable) {
+        super(UserInfo.class, forVariable(variable));
+    }
+
+    public QUserInfo(Path<? extends UserInfo> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUserInfo(PathMetadata metadata) {
+        super(UserInfo.class, metadata);
+    }
+
+}
+
